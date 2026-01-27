3f84f0131334c2cb01fe47d08b2bf61bf85bdaba
Andrea Ghezzi <andrea.ghezzi@imtek.uni-freiburg.de>
2026-01-27 13:40:50 +0100

    aligning shot dataset to camino

--- benchmark/read_shot.py
+++ benchmark/read_shot.py
@@ -35,20 +35,26 @@ def read_config(config_path):
 def get_data(name, osrl_file):
     """Get data osr file."""
     if not path.exists(osrl_file):
-        return [name, "NAN", "NAN", "NAN", "NAN"]
+        return [name, "NAN", "NAN", "NAN", "NAN", "NAN", "NAN"]
 
     tree = read_config(osrl_file)
     try:
         primal = tree.find(
             ".//other[@name='PrimalObjectiveBound']").attrib["value"]
+        dual = tree.find(
+            ".//other[@name='DualObjectiveBound']").attrib["value"]
         time_total = float(tree.find(".//time[@type='Total']").text)
         time_setup = float(
             tree.find(".//time[@type='ProblemInitialization']").text)
-        dual = tree.find(".//other[@name='TotalNumberOfDualProblems']").attrib["value"]
         nlp = tree.find(".//other[@name='NumberOfNLPProblems']").attrib["value"]
-        return [name, primal, str(time_total - time_setup), dual, nlp]
+        mip = tree.find(".//other[@name='NumberOfFeasibleMILPProblems']").attrib["value"] + \
+            tree.find(".//other[@name='NumberOfFeasibleMIQPProblems']").attrib["value"] + \
+            tree.find(".//other[@name='NumberOfOptimalMILPProblems']").attrib["value"] + \
+            tree.find(".//other[@name='NumberOfOptimalMIQPProblems']").attrib["value"]
+        relaxed_mip = tree.find(".//other[@name='NumberOfLPProblems']").attrib["value"] + tree.find(".//other[@name='NumberOfQPProblems']").attrib["value"]
+        return [name, primal, dual, str(time_total - time_setup), nlp, mip, relaxed_mip]
     except Exception:
-        return [name, "NAN", "NAN", "NAN", "NAN"]
+        return [name, "NAN", "NAN", "NAN", "NAN", "NAN", "NAN"]
 
 
 if len(argv) != 4:
@@ -62,7 +68,7 @@ with open(argv[1], "r") as f:
 
 base_path = argv[2]
 output_path = argv[3]
-data = [["id", "path", "obj", "calctime", "nr_milp", "nr_nlp"]]
+data = [["id", "path", "obj", "dual_obj", "calc_time", "NLP_runs", "MIP_runs", "relaxed_MIP_runs"]]
 for i, name in enumerate(problems):
     osrl_file = base_path + "/" + name + ".osrl"
     data.append([i] + get_data(name, osrl_file))
